5bd789c429b5d01976ca6a051437feb2b07e9327
Nick Fox-Gieg <nick@fox-gieg.com>
2023-06-16 11:57:33 -0400

    Update OpenXR_PassthroughManager.cs

--- Scripts/OpenXR_PassthroughManager.cs
+++ Scripts/OpenXR_PassthroughManager.cs
@@ -9,8 +9,19 @@ public class OpenXR_PassthroughManager : MonoBehaviour {
     private void Start() {
         switch (targetDevice.whichDevice) {
             case OpenXR_TargetDevice.WhichDevice.OCULUS:
-                var passthrough = gameObject.AddComponent<OVRPassthroughLayer>();
-                passthrough.overlayType = OVROverlay.OverlayType.Underlay;
+                //var passthrough = gameObject.AddComponent<OVRPassthroughLayer>();
+                //passthrough.overlayType = OVROverlay.OverlayType.Underlay;
+
+                // https://github.com/DazzTDuck/Quest_Passthrough_MRTK3/blob/main/Assets/Oculus/SampleFramework/Usage/Passthrough/Scripts/EnableUnpremultipliedAlpha.cs
+                // Since the alpha values for Selective Passthrough are written to the framebuffers after the color pass, we
+                // need to ensure that the color values get multiplied by the alpha value during compositing. By default, this is
+                // not the case, as framebuffers typically contain premultiplied color values. This step is only needed when
+                // Selective Passthrough is non-binary (i.e. alpha values are neither 0 nor 1), and it doesn't work if the
+                // framebuffer contains semi-transparent pixels even without Selective Passthrough, as those will have
+                // premultiplied colors.
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_ANDROID
+                OVRManager.eyeFovPremultipliedAlphaModeEnabled = false;
+#endif
                 break;
         }
     }
